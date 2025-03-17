a89ef58b9eac1967fa559439e398d434cdf3a4d3
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2025-03-17 20:45:03 -0300

    "Design do CHat sendo feito e protocolos para conexão com o banco feita"

--- app/src/main/java/com/example/prolink/Activity/ChatActivity.java
+++ app/src/main/java/com/example/prolink/Activity/ChatActivity.java
@@ -1,15 +1,10 @@
 package com.example.prolink.Activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.prolink.R;
 
 public class ChatActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-    }
 }

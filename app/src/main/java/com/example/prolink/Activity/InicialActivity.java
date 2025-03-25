4041b841111e746bf319e5dc84e9a072e85efd2d
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2025-03-25 14:58:18 -0300

    "Tela Inicial Funcional"

--- app/src/main/java/com/example/prolink/Activity/InicialActivity.java
+++ app/src/main/java/com/example/prolink/Activity/InicialActivity.java
@@ -1,7 +1,10 @@
 package com.example.prolink.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.prolink.R;
 
@@ -11,5 +14,24 @@ public class InicialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+
+        Button btnRegister = findViewById(R.id.btn_register);
+        Button btnLogin = findViewById(R.id.btn_cadastrar);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
